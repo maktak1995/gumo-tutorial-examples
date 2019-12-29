624f62a5f5a42b91e025db70731a2b0275377bfb
MakTak <justaway22@gmail.com>
2019-12-29 17:25:14 +0900

    TaskNameUpdateServiceのテストを追加

--- tests/todo/application/task/test_service.py
+++ tests/todo/application/task/test_service.py
@@ -2,7 +2,7 @@ import main
 
 from gumo.datastore.infrastructure.test_utils import DatastoreRepositoryMixinForTest
 from todo.application.task.repository import TaskRepository
-from todo.application.task import TaskCreateService, TaskStatusUpdateService
+from todo.application.task import TaskCreateService, TaskStatusUpdateService, TaskNameUpdateService
 from todo.domain import TaskKey
 
 
@@ -20,15 +20,22 @@ class TestTaskService(DatastoreRepositoryMixinForTest):
 
         assert self.count_entities() == 1
 
-    def test_update_service(self):
-        self.cleanup_entities()
-        assert self.count_entities() == 0
-
+    def test_status_update_service(self):
         task_name = "TaskName"
         create_service = main.injector.get(TaskCreateService)
         task = create_service.execute(task_name=task_name)
 
-        update_service = main.injector.get(TaskStatusUpdateService)
-        updated_task = update_service.execute(key=task.key, finished=True)
+        status_update_service = main.injector.get(TaskStatusUpdateService)
+        updated_task = status_update_service.execute(key=task.key, finished=True)
 
         assert updated_task.is_finished
+
+    def test_task_name_update_service(self):
+        task_name = "TaskName"
+        create_service = main.injector.get(TaskCreateService)
+        task = create_service.execute(task_name=task_name)
+
+        task_name_update_service = main.injector.get(TaskNameUpdateService)
+        updated_task = task_name_update_service.execute(key=task.key, task_name="NewTaskName")
+
+        assert updated_task.name.value == "NewTaskName"
